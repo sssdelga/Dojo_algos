09e98636ea2432efef8fc674d71f030c85a86a17
Samantha Delgado <sss.delgad@gmail.com>
2021-03-29 21:19:58 -0400

    Linked lists- contains completed

--- contains_linkedlists.js
+++ contains_linkedlists.js
@@ -12,27 +12,93 @@ class Node {
 
 class SLL {
     // constructor, other methods, removed for brevity
+    // SLL only needs the start point
     constructor() {
         this.head = null;
     }
     
-    add(node) {
-        var newNode = node;
-        newNode.next = null;
-        return newNode;
+    addEnd(node) {
+        if(this.head==null){
+            console.log('entered empty head if');
+            this.head=node;
+        } else {
+            var runner = this.head;
+            while(runner.val!=null){
+                console.log('***addEnd test spot***');
+                if(runner.next==null){
+                    console.log('entered end of list');
+                    runner.next=node;
+                    return this.head;
+                }
+                runner=runner.next;
+            }
+        }
+        return this.head;
+    }
+
+    add(node, value) {
+        if(this.head==null){
+            //console.log('entered first if');
+            this.head=node;
+        } else {
+            var runner=this.head;
+            while(runner.next!=null){
+                if(runner.val=value){
+                    node.next=runner.next;
+                    runner.next=node;
+                    //abstraction
+                    return this.head;
+                    }
+                }
+            }
+        return this.head;
     }
 
     contains(value) {
-    	// is "value" to be found anywhere in this list?
+        var runner=this.head;
+        var match=false;
+        while(runner.next!=null){
+            if(runner.val===value){
+                console.log('entered match if');
+                match=true;
+                break;
+            }
+            runner=runner.next;
+        // is "value" to be found anywhere in this list?
+        }
+        console.log('exit while');
+        if(match==true){
+            console.log('A match exists');
+        } else {
+            console.log('A match does not exist');
+        }
     }
 }
 
 //test
 a = new Node('test');
-console.log(a);
 firstSll = new SLL();
 firstSll.add(a);
 console.log(firstSll);
+// SLL1 = test-null
 b = new Node('second');
-firstSll.add(b);
+firstSll.addEnd(b);
 console.log(firstSll);
+// SLL2 = test-second-null
+d = new Node('third');
+console.log(d);
+firstSll.addEnd(d);
+console.log(firstSll);
+// SLL3 = test-second-third-null
+
+c = new Node('empty list');
+secondSll = new SLL();
+secondSll.addEnd(c);
+console.log(secondSll);
+
+// test contains
+console.log('*******************************');
+testVal = 'watermelon';
+firstSll.contains(testVal);
+testVal2 = 'test';
+firstSll.contains(testVal2);
